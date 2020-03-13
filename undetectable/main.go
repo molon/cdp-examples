eeb5403e945faa2d529a65d22a73dd4d287ae3da
molon <dudl@qq.com>
2020-03-13 23:49:59 +0800

    bakbak

--- undetectable/main.go
+++ undetectable/main.go
@@ -14,10 +14,12 @@ import (
 func main() {
 	ctx := context.Background()
 	allocOpts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.Flag("headless", true),
+		chromedp.Flag("headless", false),
 		chromedp.Flag("enable-automation", false),
 		chromedp.Flag("window-position", "0,0"),
-		chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"),
+		// chromedp.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"),
+		chromedp.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"),
+		// chromedp.ProxyServer("http://127.0.0.1:8888"),
 	)
 	allocCtx, allocCancel := chromedp.NewExecAllocator(ctx, allocOpts...)
 	defer allocCancel()
@@ -27,12 +29,19 @@ func main() {
 	var b1, b2, b3, b4, b5, b6 []byte
 	if err := chromedp.Run(
 		bctx,
-		chromedp.Undetectable(), // 这个只能作用于当前页而已
+		// chromedp.Undetectable(chromedp.BypassIframeTest(false)), // 这个只能作用于当前页而已
+		chromedp.Undetectable(chromedp.BypassIframeTest(true)), // 这个只能作用于当前页而已
+		chromedp.Navigate(`https://www.footaction.com/`),
+		chromedp.Sleep(2000*time.Second),
 
-		chromedp.EmulateViewport(1920, 9000),
-		chromedp.Navigate(`https://bot.sannysoft.com/`),
-		chromedp.Sleep(2*time.Second),
-		chromedp.CaptureScreenshot(&b1),
+		// chromedp.EmulateViewport(1920, 9000),
+		// chromedp.Navigate(`https://bot.sannysoft.com/`),
+		// chromedp.Sleep(2*time.Second),
+		// chromedp.CaptureScreenshot(&b1),
+
+		// chromedp.Navigate(`https://recaptcha-demo.appspot.com/recaptcha-v3-request-scores.php`),
+		// chromedp.Sleep(5000*time.Second),
+		// chromedp.CaptureScreenshot(&b1),
 
 		// chromedp.Navigate(`https://intoli.com/blog/making-chrome-headless-undetectable/chrome-headless-test.html`),
 		// chromedp.Sleep(2*time.Second),
